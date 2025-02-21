4c6742c21c3c6576352d771bf16dda1a9e9688ae
Muhammad Awais <awaisyousafzai753@gmail.com>
2025-02-21 20:29:39 +0500

    initial-commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let birthDate = Number(prompt('Enter Your Birth Year "e.g 2000"'));
+let currentYear = Number(prompt('Enter Your Birth Year "e.g 2025"'));
+let calculateAge = Number(currentYear - birthDate);
+alert("Your Age is :" + calculateAge);
+
