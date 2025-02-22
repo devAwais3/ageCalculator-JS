28cf7a32f5c70c37d1b39d1854caca8c7bdc93c3
Muhammad Awais <awaisyousafzai753@gmail.com>
2025-02-22 22:43:38 +0500

    added a function for a button(onclick) & added if/else which tells weather the age is Odd/even.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let birthDate = Number(prompt('Enter Your Birth Year "e.g 2000"'));
-let currentYear = Number(prompt('Enter Your Birth Year "e.g 2025"'));
-let calculateAge = Number(currentYear - birthDate);
-alert("Your Age is :" + calculateAge);
+function age() {
+    let birthDate = Number(prompt('Enter Your Birth Year "e.g 2000"'));
+    let currentYear = Number(prompt('Enter Your Birth Year "e.g 2025"'));
+    let calculateAge = Number(currentYear - birthDate);
+
+    if (calculateAge % 2 === 0) {
+        alert("Your Age is : " + calculateAge + " Years" + "\nAge is Even");
+    }
+    else {
+        alert("Your Age is : " + calculateAge + " Years" + "\nAge is Odd");
+    }
+}
+
 
