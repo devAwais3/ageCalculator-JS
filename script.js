3f755175ec6777d36bea35ddeb9c5dd63acd4b58
Muhammad Awais <awaisyousafzai753@gmail.com>
2025-02-23 07:03:26 +0500

    corrected prompt for (let currentYear).

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function age() {
     let birthDate = Number(prompt('Enter Your Birth Year "e.g 2000"'));
-    let currentYear = Number(prompt('Enter Your Birth Year "e.g 2025"'));
+    let currentYear = Number(prompt('Enter Current Year "e.g 2025"'));
     let calculateAge = Number(currentYear - birthDate);
 
     if (calculateAge % 2 === 0) {
